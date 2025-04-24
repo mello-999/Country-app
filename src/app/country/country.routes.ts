1e32e9b2f750dde13893f8a6769a644a54365fed
Mauricio donoso <donosomauricio721@gmail.com>
2025-04-24 13:57:15 -0400

    Cap.108-Listo

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,28 +1,29 @@
 import {  Routes } from "@angular/router";
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { CountryLayoutComponent } from "./layouts/CountryLayout/CountryLayout.component";
 
 
 export const countryRoutes: Routes = [
 
   {
     path:'',
-    component: ByCapitalPageComponent,
-    // component: HomePageComponent,
+    component: CountryLayoutComponent,
+    children:[
+      {
+        path:'by-capital',
+        component: ByCapitalPageComponent,
+      },
+
+          {
+            path:'**',
+            redirectTo: 'by-capital',
+          },
+    ],
   },
-
-  // {
-  //   path: 'country',
-  //   /// ??? {}
-
-  // },
-
-  // {
-  //   path:'**',
-  //   redirectTo: '',
-  // },
-
 
   ];
 
-export default countryRoutes;
+  export default countryRoutes;
+
+
 
--- src/app/shared/home-page/home-page.component.ts
+++
@@ -1,9 +0,0 @@
-mport { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home-page',
-  imports: [],
-  templateUrl: './home-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class HomePageComponent { }
