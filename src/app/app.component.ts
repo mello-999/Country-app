6c55af81503231062a21676385dd84a92fa14a75
Mauricio donoso <donosomauricio721@gmail.com>
2025-04-25 03:14:07 -0400

    Cap.109-Listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
+  imports: [RouterModule, FooterComponent],
   templateUrl: './app.component.html',
-  imports: [CommonModule, RouterModule],
 })
-export class AppComponent implements OnInit {
-  title = 'Previsualización de Temas DaisyUI';
-
-  themes: string[] = [
-    "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter", "dim", "nord", "sunset", "caramelatte", "abyss", "silk"
-  ];
-
-  currentThemeIndex = 0;
-
-  ngOnInit(): void {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-      const index = this.themes.indexOf(savedTheme);
-      this.currentThemeIndex = index >= 0 ? index : 0;
-      this.applyTheme(this.themes[this.currentThemeIndex]);
-    }
-  }
-
-  cambiarTema(): void {
-    this.currentThemeIndex = (this.currentThemeIndex + 1) % this.themes.length;
-    const newTheme = this.themes[this.currentThemeIndex];
-    this.applyTheme(newTheme);
-  }
-
-  applyTheme(theme: string): void {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-  }
+export class AppComponent {}
 
 
-}
