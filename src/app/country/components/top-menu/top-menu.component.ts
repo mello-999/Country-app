5336d6cb00f074469c26b73a336c4e44fd024486
Mauricio donoso <donosomauricio721@gmail.com>
2025-04-29 12:57:10 -0400

    Cap.110-Avance

---
+++ src/app/country/components/top-menu/top-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-top-menu',
+  imports: [],
+  templateUrl: './top-menu.component.html',
+})
+export class TopMenuComponent { }
