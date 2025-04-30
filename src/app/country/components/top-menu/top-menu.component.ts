6c0082dc9713e3be3790b47952be69c8118352b4
Mauricio donoso <donosomauricio721@gmail.com>
2025-04-30 02:03:45 -0400

    Cap.110-Listo

--- src/app/country/components/top-menu/top-menu.component.ts
+++ src/app/country/components/top-menu/top-menu.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'country-top-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './top-menu.component.html',
 })
 export class TopMenuComponent { }
