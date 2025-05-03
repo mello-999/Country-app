cdd8e84c3e4e00339c292e7cb416c8d86921a646
Mauricio donoso <donosomauricio721@gmail.com>
2025-05-02 23:07:01 -0400

    Sección-10-Cap.119Listo

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import {  Component } from '@angular/core';
+import {  Component, inject } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
 
 
 
@@ -11,11 +12,18 @@ import { CountryListComponent } from "../../components/country-list/country-list
   templateUrl: './by-capital-page.component.html',
  })
 export class ByCapitalPageComponent {
-  onSearch (value: string) {
-    console.log({value});
-  }
+  CountryService = inject(CountryService);
 
+
+  onSearch (query: string) {
+      this.CountryService.searchBycapital(query).subscribe((resp) => {
+         console.log(resp);
+});
+
+  }
 }
+
+
 
 
 
